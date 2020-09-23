af9e310b07328eca7148ef22dbcdd95e814c511c
armmieyy <68525381+armmieyy@users.noreply.github.com>
2020-09-23 22:57:44 +0700

    init now

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,26 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <meta charset="utf-8" />
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1, shrink-to-fit=no"
+    />
+    <img
+      src="https://cdn.discordapp.com/attachments/748191031870226462/758350643710132224/dbt4epd-5198725c-78be-4fc5-8100-ce8ede877ac3.gif"
+      class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto"
+    />
+    <anime />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import anime from "./components/anime";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    anime,
+  },
+};
 </script>
 
 <style>
@@ -24,5 +31,12 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+body {
+  background-image: url("https://cdn.discordapp.com/attachments/748191031870226462/758349632807239691/934905.jpg");
+  background-attachment: fixed;
+  background-repeat: no-repeat;
+  background-size: 100% 100%;
+  height: 100vh;
 }
 </style>
